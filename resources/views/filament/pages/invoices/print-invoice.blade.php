cb91005cceaf63cd0178782a1e39fa00f26fa5c8
kareemRafat <kareem.force@gmail.com>
2025-10-28 17:18:22 +0300

    fix customer name in printable test invoice blade

--- resources/views/filament/pages/invoices/print-invoice.blade.php
+++ resources/views/filament/pages/invoices/print-invoice.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>فاتورة {{$customer->name }}</title>
+    <title>فاتورة</title>
     <style>
         * {
             font-family: 'DejaVu Sans', sans-serif;
